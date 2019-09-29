78fb42ce7f4b091523f6133ee022f1dd097c967f
Johnathan Moes <johnman747@gmail.com>
2019-09-29 14:57:49 -0500

    added more material ui and better functionality

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,89 +1,90 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import {connect} from 'react-redux';
-import {HashRouter as Router} from 'react-router-dom';
-import {Table, TableBody, TableCell, TableHead, TableRow,Button} from '@material-ui/core'
+import { connect } from 'react-redux';
+import { HashRouter as Router } from 'react-router-dom';
+import { Table, TableBody, TableCell, TableHead, TableRow, Button } from '@material-ui/core'
 import './Admin.css'
 
-class  Admin extends Component {
-    state={
+class Admin extends Component {
+    state = {
         feedback: []
     }
-    componentDidMount(){
+    componentDidMount() {
         this.getFeedback();
     }
 
-    getFeedback = ()=>{
+    getFeedback = () => {
         axios.get('/feedback')
-        .then((result)=>{
-            this.setState({
-                feedback: result.data.rows
-            })
-        }).catch((err)=>{
-            console.log(err);
-        })
-    }
-
-    deleteBtn = (id)=>{
-        ;
-        if(window.confirm('Are you sure?')){
-            axios.delete('/feedback/'+ id)
-            .then((result)=>{
-                this.getFeedback();
-            }).catch((err)=>{
+            .then((result) => {
+                this.setState({
+                    feedback: result.data.rows
+                })
+            }).catch((err) => {
                 console.log(err);
             })
+    }
+
+    deleteBtn = (id) => {
+        if (window.confirm('Are you sure?')) {
+            axios.delete('/feedback/' + id)
+                .then((result) => {
+                    this.getFeedback();
+                }).catch((err) => {
+                    console.log(err);
+                })
         }
-      
+
     }
 
-    handleFlag = (boolean, id)=>{
+    handleFlag = (boolean, id) => {
         let thing = !boolean;
-        axios.put('/feedback/'+ id +"/"+thing)
-        .then((result)=>{
-            this.getFeedback();
-        }).catch((err)=>{
-            console.log(err);
-        })
+        axios.put('/feedback/' + id + "/" + thing)
+            .then((result) => {
+                this.getFeedback();
+            }).catch((err) => {
+                console.log(err);
+            })
     }
 
-render() {
-    return (
-    <Router>
-        <h1>Admin Page</h1>
-        <Table>
-            <TableHead className='tableHead'>
-                <TableRow>
-                    <TableCell>Feelings</TableCell>
-                    <TableCell>Understanding</TableCell>
-                    <TableCell>Support</TableCell>
-                    <TableCell>Comments</TableCell> 
-                    <TableCell>Flag for Review</TableCell> 
-                    <TableCell>Delete</TableCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                {this.state.feedback.map((item)=>{
-                    return(<TableRow key={item.id}>
-                        <TableCell>{item.feeling}</TableCell>
-                        <TableCell>{item.understanding}</TableCell>
-                        <TableCell>{item.support}</TableCell>
-                        <TableCell>{item.comments}</TableCell>
-                        {item.flagged?
-                        <TableCell><Button variant="contained" className="flagged" onClick={()=>this.handleFlag(item.flagged, item.id)}>Unflag</Button></TableCell>:
-                        <TableCell><Button variant="contained" onClick={()=>this.handleFlag(item.flagged, item.id)}>Flag</Button></TableCell>
-                        }
-                        <TableCell><Button variant="contained" onClick={()=>this.deleteBtn(item.id)}>Delete</Button></TableCell>
-                    </TableRow>)
+    render() {
+        return (
+            <Router>
+                <h1>Admin Page</h1>
+                <div className="tableSize">
+                    <Table stickyHeader>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Feelings</TableCell>
+                                <TableCell>Understanding</TableCell>
+                                <TableCell>Support</TableCell>
+                                <TableCell>Comments</TableCell>
+                                <TableCell>Flag for Review</TableCell>
+                                <TableCell>Delete</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {this.state.feedback.map((item) => {
+                                return (<TableRow key={item.id}>
+                                    <TableCell>{item.feeling}</TableCell>
+                                    <TableCell>{item.understanding}</TableCell>
+                                    <TableCell>{item.support}</TableCell>
+                                    <TableCell>{item.comments}</TableCell>
+                                    {item.flagged ?
+                                        <TableCell className="flagged"><Button variant="contained" onClick={() => this.handleFlag(item.flagged, item.id)}>Unflag</Button></TableCell> :
+                                        <TableCell><Button variant="contained" onClick={() => this.handleFlag(item.flagged, item.id)}>Flag</Button></TableCell>
+                                    }
+                                    <TableCell><Button variant="contained" onClick={() => this.deleteBtn(item.id)}>Delete</Button></TableCell>
+                                </TableRow>)
 
-                })}
-            </TableBody>
-        </Table>
-    </Router>
-    );
-  }
+                            })}
+                        </TableBody>
+                    </Table>
+                </div>
+            </Router>
+        );
+    }
 }
 const mapStateToProps = (reduxStore) => ({
     reduxStore
-  })
+})
 export default connect(mapStateToProps)(Admin);
